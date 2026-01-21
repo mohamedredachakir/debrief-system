49db4546e19599621779e3cdaf0df8ac03e6e3a9
reddcs <chakirm082@gmail.com>
2026-01-21 12:42:58 +0100

    feat: Create database.php

---
+++ config/database.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'default' => $_ENV['DB_CONNECTION'] ?? 'pgsql',
+    'connections' => [
+        'pgsql' => [
+            'driver' => 'pgsql',
+            'host' => $_ENV['DB_HOST'] ?? '127.0.0.1',
+            'port' => $_ENV['DB_PORT'] ?? '5432',
+            'database' => $_ENV['DB_DATABASE'] ?? 'debrief_db',
+            'username' => $_ENV['DB_USERNAME'] ?? 'postgres',
+            'password' => $_ENV['DB_PASSWORD'] ?? '',
+            'charset' => 'utf8',
+            'prefix' => '',
+            'schema' => 'public',
+        ],
+    ],
+];
