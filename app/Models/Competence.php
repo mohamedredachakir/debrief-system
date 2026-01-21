9ff0aa8402132b84bf1f43f3080bb16959ddd3a4
reddcs <chakirm082@gmail.com>
2026-01-21 12:42:57 +0100

    feat: Create Competence.php

---
+++ app/Models/Competence.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+class Competence extends Model
+{
+    protected $table = 'competences';
+    
+    public int $id;
+    public string $code;
+    public string $label;
+
+    public function __construct(int $id = 0, string $code = '', string $label = '')
+    {
+        $this->id = $id;
+        $this->code = $code;
+        $this->label = $label;
+    }
+
+    public static function all()
+    {
+        $db = \App\Core\Database::getInstance();
+        if ($db->isMock()) {
+            $c1 = new self(); $c1->id=1; $c1->code='C1'; $c1->label='Maquetter une application';
+            $c2 = new self(); $c2->id=2; $c2->code='C2'; $c2->label='Développer une interface web';
+            $c3 = new self(); $c3->id=3; $c3->code='C3'; $c3->label='Développer la partie back-end';
+            return [$c1, $c2, $c3];
+        }
+        return self::fetchAll("SELECT * FROM competences ORDER BY code ASC", [], self::class);
+    }
+}
