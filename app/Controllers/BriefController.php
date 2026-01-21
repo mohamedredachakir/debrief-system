32eebd7b993234d9ad5dc2808cbcb9676167908a
reddcs <chakirm082@gmail.com>
2026-01-21 12:42:57 +0100

    feat: Create BriefController.php

---
+++ app/Controllers/BriefController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Core\Controller;
+use App\Services\StructureService;
+
+class BriefController extends Controller
+{
+    private $service;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->service = new StructureService();
+    }
+
+    public function index()
+    {
+        $sprintId = $_GET['sprint_id'] ?? null;
+        if (!$sprintId) {
+            $this->redirect('/admin/classes');
+            return;
+        }
+
+        $briefs = $this->service->getBriefsForSprint($sprintId);
+        $this->view('admin.briefs.index', ['briefs' => $briefs, 'sprint_id' => $sprintId]);
+    }
+}
