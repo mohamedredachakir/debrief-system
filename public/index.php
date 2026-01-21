1f9f4c5d71d1aab5d5b00c27f5efe8cbd99c94e4
reddcs <chakirm082@gmail.com>
2026-01-21 16:19:11 +0100

    Update public index.php with application entry point

--- public/index.php
+++ public/index.php
@@ -2,20 +2,14 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-
-
 use Dotenv\Dotenv;
 use App\Core\Router;
 
-// Load .env
 $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
-// Initialize Router
 $router = new Router();
 
-// Load Routes
 require_once __DIR__ . '/../routes/web.php';
 
-// Dispatch
 $router->run();
