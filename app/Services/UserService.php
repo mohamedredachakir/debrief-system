30f0110f4876bddc6dac9a91b74e07a4313527a4
reddcs <chakirm082@gmail.com>
2026-01-21 12:42:58 +0100

    feat: Create UserService.php

---
+++ app/Services/UserService.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\UserRepository;
+
+class UserService
+{
+    private $userRepo;
+
+    public function __construct()
+    {
+        $this->userRepo = new UserRepository();
+    }
+
+    public function getAllUsers()
+    {
+        return $this->userRepo->all();
+    }
+
+    public function getLearnersByClass($classId)
+    {
+        return $this->userRepo->getByClass($classId, 'learner');
+    }
+    
+    public function getUser($id)
+    {
+        return $this->userRepo->find($id);
+    }
+
+    public function createUser($data)
+    {
+        // Hash password if provided
+        if (isset($data['password'])) {
+            $data['password'] = password_hash($data['password'], PASSWORD_BCRYPT);
+        }
+        return $this->userRepo->create($data);
+    }
+
+    public function updateUser($id, $data)
+    {
+         if (!empty($data['password'])) {
+            $data['password'] = password_hash($data['password'], PASSWORD_BCRYPT);
+        } else {
+            unset($data['password']); // Don't overwrite if empty
+        }
+        return $this->userRepo->update($id, $data);
+    }
+}
