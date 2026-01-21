948f3cd74e306071a1c6f8fadcae323c5b76b334
reddcs <chakirm082@gmail.com>
2026-01-21 16:19:11 +0100

    Update RepositoryInterface with standard methods

--- app/Repositories/RepositoryInterface.php
+++ app/Repositories/RepositoryInterface.php
@@ -7,6 +7,5 @@ interface RepositoryInterface
     public function all();
     public function find($id);
     public function create(array $data);
-    // public function update($id, array $data);
-    // public function delete($id);
+
 }
