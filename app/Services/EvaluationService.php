224ba620f8af38a04a4afd51f6872d7d3fabcc6d
reddcs <chakirm082@gmail.com>
2026-01-21 12:42:58 +0100

    feat: Create EvaluationService.php

---
+++ app/Services/EvaluationService.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\EvaluationRepository;
+
+class EvaluationService
+{
+    private $evalRepo;
+
+    public function __construct()
+    {
+        $this->evalRepo = new EvaluationRepository();
+    }
+
+    public function submitEvaluation($data)
+    {
+        // Could wrap in transaction or loop here if not already handled
+        return $this->evalRepo->create($data);
+    }
+}
