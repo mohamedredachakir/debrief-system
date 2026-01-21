1ad512cc13a7a7787ac212dd2ec8cab4969defc6
reddcs <chakirm082@gmail.com>
2026-01-21 12:42:58 +0100

    feat: Create AuthService.php

---
+++ app/Services/AuthService.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\UserRepository;
+
+class AuthService
+{
+    private $userRepo;
+
+    public function __construct()
+    {
+        $this->userRepo = new UserRepository();
+    }
+
+    public function authenticate($email, $password)
+    {
+        $user = $this->userRepo->findByEmail($email);
+        
+        if ($user && password_verify($password, $user->password)) {
+            return $user;
+        }
+        return null;
+    }
+}
